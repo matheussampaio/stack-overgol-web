8e69bf7c0c39d061ce5088cb84030fb8cd1f39e6
Matheus Sampaio <matheus1401@gmail.com>
2018-03-21 00:23:49 -0600

    feat: add number format and fix debounce issue

--- src/components/TableCell.vue
+++ src/components/TableCell.vue
@@ -4,7 +4,7 @@
     <input v-if="isCheckboxField" @input="onInput" @change="onInput" type="checkbox" v-model="newValue">
 
     <!-- Number -->
-    <input v-else-if="isNumberField" @input="onInput" @change="onInput" type="number" v-model.number="newValue">
+    <input v-else-if="isNumberField" @input="onInput" @change="onInput" type="number" v-model.number="formattedNumber">
 
     <!-- Text -->
     <input v-else @input="onInput" @change="onInput" type="text" v-model="newValue">
@@ -23,9 +23,15 @@ export default {
   },
   methods: {
     // eslint-disable-next-line func-names
-    onInput: debounce(function () {
-      this.$emit('onFieldChanged', { newValue: this.newValue })
-    }, 2000)
+    onInput() {
+      if (this.onInputDebounced == null) {
+        this.onInputDebounced = debounce(() => {
+          this.$emit('onFieldChanged', { newValue: this.newValue })
+        }, 2000)
+      }
+
+      this.onInputDebounced()
+    }
   },
   computed: {
     isCheckboxField() {
@@ -33,6 +39,19 @@ export default {
     },
     isNumberField() {
       return this.type === 'number'
+    },
+    formatter() {
+      const options = { minimumIntegerDigits: 1, minimumFractionDigits: 2, maximumFractionDigits: 2 }
+
+      return new Intl.NumberFormat('arab', options)
+    },
+    formattedNumber: {
+      get() {
+        return this.formatter.format(this.newValue)
+      },
+      set(v) {
+        this.newValue = v
+      }
     }
   }
 }
