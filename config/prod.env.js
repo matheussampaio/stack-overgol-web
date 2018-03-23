a65c25959b045101f00ea8bf3fdcc58dea38d0c5
Matheus Sampaio <matheus1401@gmail.com>
2018-03-22 22:57:29 -0600

    chore: add dotenv

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,11 @@
 'use strict'
 
+const dotenv = require('dotenv')
+
+dotenv.config()
+
 module.exports = {
+  FIREBASE_API_KEY: `"${process.env.FIREBASE_API_KEY}"`,
+  FIREBASE_APP_NAME: `"${process.env.FIREBASE_APP_NAME}"`,
   NODE_ENV: '"production"'
 }
--- src/helpers/firebaseConfig.js
+++ src/helpers/firebaseConfig.js
@@ -1,9 +1,9 @@
 const config = {
-  apiKey: 'AIzaSyCN44MXJmFQW42p8VIn7UXn7PxCf2rnVFM',
-  authDomain: 'stack-overgol-bot.firebaseapp.com',
-  databaseURL: 'https://stack-overgol-bot.firebaseio.com',
-  projectId: 'stack-overgol-bot',
-  storageBucket: 'stack-overgol-bot.appspot.com',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: `${process.env.FIREBASE_APP_NAME}.firebaseapp.com`,
+  databaseURL: `https://${process.env.FIREBASE_APP_NAME}.firebaseio.com`,
+  projectId: `${process.env.FIREBASE_APP_NAME}`,
+  storageBucket: `${process.env.FIREBASE_APP_NAME}.appspot.com`,
   messagingSenderId: '564241331900'
 }
 
